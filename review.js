15e53b26d897eb76feb11a182cac55685e716e71
mukhtorov <mukhtorov_1994@mail.ru>
2023-01-23 19:02:55 +0500

    meni qoshildi

--- review.js
+++ review.js
@@ -1,11 +1,24 @@
 // repository - project
 
+// config
+// git config --global user.email "webbrain@gmail.com"
+// git config --global user.name "webbrain"
+
 // connection
 // 1. git init
 // 2. git remote add origin https://github.com/mukhtorov/g11.git
 
-// branch
 // push
+// 1. git add .
+// 2. git commit -m 'test'
+// 3. git push origin master
+
 // pull
+// 1. git pull origin master
+
+// branch
+// git branch branchName
+// git checkout branchName
+// git branch -m branchName
 
 // wba_students
